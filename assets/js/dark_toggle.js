6a56207304c775026e75ff6d5daf769182f8d89e
W Zhang <39925558+WeixuanZ@users.noreply.github.com>
2020-05-16 01:18:08 +0100

    Desktop default to light theme independent of the preference

--- assets/js/dark_toggle.js
+++ assets/js/dark_toggle.js
@@ -1,19 +1,21 @@
-var is_dark = window.matchMedia('(prefers-color-scheme)').matches;
+var is_dark = window.matchMedia('(prefers-color-scheme)').matches && window.matchMedia("(max-width: 1000px)").matches;
 var maxScroll = 0;
 var minScroll = 0;
 const dark_style = '<link rel="stylesheet" type="text/css" href="/assets/css/dark.css">';
 
-// if (is_dark) {
-//     $( 'head' ).append(dark_style)      
-// }
+if (is_dark) {
+    $( 'head' ).append(dark_style)      
+}
 
 function toggle() {
     if (maxScroll < 2 * nav_init) {
         if (document.documentElement.scrollTop >= 0 && minScroll < -40) {
             if (is_dark) {
                 $( 'head link' ).last().remove();
+                is_dark = false;
             } else {
                 $( 'head' ).append(dark_style);
+                is_dark = true;
             };
             minScroll = 0;
             maxScroll = 0;
