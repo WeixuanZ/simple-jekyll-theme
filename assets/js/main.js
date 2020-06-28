5119cded49f669b82e1e53adb886224965f15079
W Zhang <39925558+WeixuanZ@users.noreply.github.com>
2020-06-28 23:24:30 +0100

    Revert to not using defer for correct executing order Tidying up some JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-// Footer
+/* W Zhang
+ * For weixuanz.github.io
+ */
 
-const license_notice = '<a href="/license">License and Privacy Notice</a>'
-
+/* Footer */
+const license_notice = '<a href="/license">License and Privacy Notice</a>';
 if (location.href.includes("about")) {
-    $( "#copyleft" ).html(license_notice)
+    $( "#copyleft" ).html(license_notice);
 } else if (location.href.includes("license") === false) {
     $( "#main-footer" ).hover(
         function() {
@@ -18,31 +20,22 @@ if (location.href.includes("about")) {
 } ;
 
 
-// Code Copy
-
-// get all <code> elements
-var allCodeBlocksElements = $( "div.highlighter-rouge" );
-
+/* Code Copy
+ * Modified, from https://stackoverflow.com/a/48078807/10365842
+ */
+let allCodeBlocksElements = $( "div.highlighter-rouge" );
 allCodeBlocksElements.each(function(i) {
-    // add different id for each code block
-
-    // target   
-    var currentId = "codeblock" + (i + 1);
+    let currentId = `codeblock${i + 1}`;
     $("code").attr('id', currentId);
-     
-    // trigger
-    var clipButton = '<button class="code-copy-btn" data-clipboard-target="#' + currentId + '"><i class="fas fa-clone"></i>';
+    let clipButton = `<button class="code-copy-btn" data-clipboard-target="#${currentId}"><i class="fas fa-clone"></i>`;
     $("code").after(clipButton);
 });
- 
-var clipboard = new ClipboardJS('.code-copy-btn');
-
-clipboard.on('success', function(e) {
+let clipboard = new ClipboardJS('.code-copy-btn');
+clipboard.on('success', e => {
     console.log('Action:', e.action);
     console.log('Text:', e.text);
 });
-
-clipboard.on('error', function(e) {
+clipboard.on('error', e => {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
     console.log(e);
