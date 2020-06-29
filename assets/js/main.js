33115cee1cebcbd56b72dd6ee17f5e9a5fb24d83
W Zhang <39925558+WeixuanZ@users.noreply.github.com>
2020-06-29 21:47:33 +0100

    Code for theme changing, without animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ if (location.href.includes("about")) {
                 $(this).html('Copyleft <span style="display: inline-block; transform: rotate(180deg);">&copy;</span> 2020 W Zhang').fadeIn(150);
             });
     });
-} ;
+};
 
 
 /* Code Copy
@@ -43,25 +43,108 @@ clipboard.on('error', e => {
 
 
 /* Dark Toggle
- * Modified from https://www.kooslooijesteijn.net/blog/add-dark-mode-to-website
+ * Modified, from https://gist.github.com/kslstn/20f654fd27eb29619040c74fa6526919
  */
 
-const toggle_label = document.querySelector('#dark-toggle-label');
-window.addEventListener('scroll', function () {
-    if (this.scrollY >= nav_init) {
-        toggle_label.classList.add('hidden');
-    } else {
-        toggle_label.classList.remove('hidden');
-    }
-})
-const root = document.documentElement
-const checkbox = document.querySelector('#dark-toggle-box');
-checkbox.addEventListener( 'change', function() {
-    if(this.checked) {
-        root.classList.add('theme-dark');
-    } else {
+(function initializeTheme(){
+    syncBetweenTabs()
+    listenToOS()
+    listenToCheckbox()
+    hideCheckbox()
+
+    updatePref(
+        storedPref() ||
+        OSPref(),
+        false
+    )
+}())
+
+// Helper functions
+
+function currentPref() {
+    return (document.querySelector('#dark-toggle-box').checked) ? 'dark' : 'light';
+}
+
+function OSPref() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+function storedPref() {
+    const pref = localStorage.getItem('preference-theme')
+    const lastChanged = localStorage.getItem('preference-theme-last-change')
+    let now = new Date()
+    const minutesPassed = (now.getTime() - lastChanged)/(1000*60)
+
+    if (minutesPassed < 120 && pref === "light") return 'light'
+    else if (minutesPassed < 120 && pref === "dark") return 'dark'
+    else return undefined
+}
+
+function updateCheckbox(pref) {
+    document.querySelector('#dark-toggle-box').checked = (pref === 'dark');
+}
+
+function savePref(key, value){
+    let now = new Date();
+    now = now.getTime();
+    localStorage.setItem(key, value);
+    localStorage.setItem(key+"-last-change", now);
+}
+
+
+function updatePref(newPref, withTransition = false, save = true, override = false){
+    const root = document.documentElement
+    if ((currentPref() === 'dark' || override) && newPref === 'light') {
         root.classList.remove('theme-dark');
+        updateCheckbox('light');
+    } else if ((currentPref() === 'light' || override) && newPref === 'dark') {
+        root.classList.add('theme-dark');
+        updateCheckbox('dark');
     }
-});
+    if (save) savePref('preference-theme', newPref);
+}
 
 
+// Event Listeners
+
+function hideCheckbox() {
+    const toggle_label = document.querySelector('#dark-toggle-label');
+    window.addEventListener('scroll', function () {
+        if (this.scrollY >= nav_init) {
+            toggle_label.classList.add('hidden');
+        } else {
+            toggle_label.classList.remove('hidden');
+        }
+    })
+}
+
+function syncBetweenTabs(){
+    window.addEventListener('storage', (e) => {
+        if (e.key === 'preference-theme'){
+            if (e.newValue === 'light') updatePref('light', true, false)
+            else if (e.newValue === 'dark') updatePref('dark', true, false)
+        }
+    })
+}
+
+function listenToOS(){
+    let mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    mediaQueryList.addListener( (m)=> {
+        if (m.matches !== true) {
+            updatePref('light', true)
+        } else {
+            updatePref('dark', true)
+        }
+    })
+}
+
+function listenToCheckbox(){
+    const checkbox = document.querySelector('#dark-toggle-box');
+    checkbox.addEventListener('change', function() {
+        if(this.checked) {
+            updatePref('dark', true, true, true);
+        } else {
+            updatePref('light', true, true, true);
+        }
+    })
+}
